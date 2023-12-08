0b7f60b995d82d06fd37aad9c1bda0c62d4498e1
Chris <wyfj78@outlook.com>
2023-12-08 20:55:55 +0800

    First Version

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 
+
 const app = express();
 const port = 3000;
+
 
 
 const API_Url = "https://api.potterdb.com/v1/";
@@ -13,52 +15,40 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
+app.get("/", (req,res)=>{
+    res.render("index.ejs");
+})
 
-app.get("/", async(req,res)=>{
-    try{ const response1 = await axios.get(API_Url+"characters");
-         const characters = response1.data.data;
-         const character = characters[Math.floor(Math.random()*characters.length)];
-         const imgCha = character.attributes.image;
-         while(!imgCha){response1();}
+app.post("/answer", async(req,res)=>{
+    try {
+        const response1 = await axios.get(API_Url + "characters");
+        const characters = response1.data.data;
+        let character = characters[Math.floor(Math.random() * characters.length)];
+        let imgCha = character.attributes.image;
+    
+        while (!imgCha) {
+          const response2 = await axios.get(API_Url + "characters");
+          const characters2 = response2.data.data;
+          character = characters2[Math.floor(Math.random() * characters2.length)];
+          imgCha = character.attributes.image;
+        }
+ 
 
-         const response2= await axios.get(API_Url+"potions");
-         const potions = response2.data.data;
-         const potion = potions[Math.floor(Math.random()*potions.length)];
-         const imgPo = potion.attributes.image;
-         if(!imgPo){response2();}
-
-         const response3= await axios.get(API_Url+"spells");
-         const spells = response3.data.data;
-         const spell = spells[Math.floor(Math.random()*spells.length)];
-         const imgSpell = spell.attributes.image;
-         if(!imgSpell){response3();}
-
-         const response4= await axios.get(API_Url+"books");
-         const books = response4.data.data;
-         const book = books[Math.floor(Math.random()*books.length)];
-         
-
-         const response5= await axios.get(API_Url+"movies");
-         const movies = response5.data.data;
-         const movie = movies[Math.floor(Math.random()*movies.length)];
-         
-
-        res.render("index.ejs", {
+        res.render("answer.ejs", {
             characterName: character.attributes.name, 
             imgCha: imgCha,
-            chaWiki: character.attributes.wiki,
-
-            potionName: potion.attributes.name,
-            imgPo:imgPo,
-
-            spellName: spell.attributes.name,
-            imgSpell:imgSpell,
-            spellEffect:spell.attributes.effect,
-
-            movieName: movie.attributes.title,
-            imgMovie:movie.attributes.poster,
-
-            bookName: book.attributes.title,      
+            Alias_names: character.attributes.alias_names,
+            Patronus:character.attributes.patronus,
+            Animagus: character.attributes.animagus,
+            Boggart: character.attributes.boggart,
+            Jobs:character.attributes.jobs,
+            Wands:character.attributes.wands,
+            Eye_color: character.attributes.eye_color,
+            Hair_color: character.attributes.hair_color,
+            House:character.attributes.houe, 
+            Family_members: character.attributes.family_members,
+            Romances:character.attributes.romances,        
+            
          });
 
     }catch(error){
